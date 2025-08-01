9a02e6081542ce94d0ff21164b06f89044cd7f7c
harsh388 <harshtandon506@gmail.com>
2025-08-01 22:36:00 +0530

    Fourth commit

--- unzip.py
+++ unzip.py
@@ -1,16 +1,24 @@
 import gzip
 import pickle
 
-# Unzip and load the model
-model_file_path = "model.pkl.gz"
-with gzip.open(model_file_path, "rb") as f:
-    loaded_model = pickle.load(f)
+# Decompress and load the model from .gz
+model_gz_path = "model.pkl.gz"
+with gzip.open(model_gz_path, "rb") as f_in:
+    model_obj = pickle.load(f_in)
 
-print("Loaded model type:", type(loaded_model))
+# Save the model as an uncompressed .pkl
+with open("model.pkl", "wb") as f_out:
+    pickle.dump(model_obj, f_out)
 
-# Unzip and load the preprocessor
-preprocessor_file_path = "preprocessor.pkl.gz"
-with gzip.open(preprocessor_file_path, "rb") as f:
-    loaded_preprocessor = pickle.load(f)
+print("Model saved uncompressed as model.pkl")
 
-print("Loaded preprocessor type:", type(loaded_preprocessor))
+# Decompress and load the preprocessor from .gz
+preprocessor_gz_path = "preprocessor.pkl.gz"
+with gzip.open(preprocessor_gz_path, "rb") as f_in:
+    preprocessor_obj = pickle.load(f_in)
+
+# Save the preprocessor as an uncompressed .pkl
+with open("preprocessor.pkl", "wb") as f_out:
+    pickle.dump(preprocessor_obj, f_out)
+
+print("Preprocessor saved uncompressed as preprocessor.pkl")
